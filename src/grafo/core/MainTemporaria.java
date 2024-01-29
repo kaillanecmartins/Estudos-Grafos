6178369cae61e6f7f9b677075bf9a032c3e37c6f
Kaillane <kaillanemartins18@gmail.com>
2024-01-29 10:12:08 -0300

    implementando busca em profundidade

--- src/grafo/core/MainTemporaria.java
+++ src/grafo/core/MainTemporaria.java
@@ -1,6 +1,8 @@
 package grafo.core;
 
 import java.util.List;
+import grafo.search.BuscaEmLargura;
+import grafo.search.BuscaEmProfundidade;
 
 public class MainTemporaria {
     public static void main(String[] args) throws Exception {
@@ -10,16 +12,35 @@ public class MainTemporaria {
         g.adicionarVertice("B");
         g.adicionarVertice("C");
         g.adicionarVertice("D");
-        
+        g.adicionarVertice("E");
+        g.adicionarVertice("F");
+        g.adicionarVertice("G");
+        g.adicionarVertice("H");
+        g.adicionarVertice("I");
         
         g.conectarVertices("A", "B");
         g.conectarVertices("A", "C");
         g.conectarVertices("A", "D");
+        g.conectarVertices("B", "F");
+        g.conectarVertices("B", "I");
+        g.conectarVertices("D", "E");
+        g.conectarVertices("D", "I");
+        g.conectarVertices("D", "G");
+        g.conectarVertices("I", "A");
+        g.conectarVertices("I", "D");
+        g.conectarVertices("I", "C");
+        g.conectarVertices("I", "H");
+        g.conectarVertices("E", "A");
         
         System.out.println("Grau do vértice A: " + g.getVertice("A").getGrau());
         System.out.println("Grau do vértice B: " + g.getVertice("B").getGrau());
         System.out.println("Grau do vértice C: " + g.getVertice("C").getGrau());
         System.out.println("Grau do vértice D: " + g.getVertice("D").getGrau());
+        System.out.println("Grau do vértice E: " + g.getVertice("E").getGrau());
+        System.out.println("Grau do vértice F: " + g.getVertice("F").getGrau());
+        System.out.println("Grau do vértice G: " + g.getVertice("G").getGrau());
+        System.out.println("Grau do vértice H: " + g.getVertice("H").getGrau());
+        System.out.println("Grau do vértice I: " + g.getVertice("I").getGrau());
         
         System.out.println("\nO vértice A possui as seguintes adjacências: ");
         List<Vertice> adjacentes = g.getAdjacencias("A");
@@ -33,5 +54,20 @@ public class MainTemporaria {
             System.out.println(vertice.getRotulo() + " ");
         }
         
+        System.out.println("");
+        
+        List<String> caminho = BuscaEmProfundidade.getInstance().buscar(g, "D", "H");
+        System.out.print("Caminho feito por uma busca em profundidade: ");
+        for(String passo : caminho) {
+         System.out.print(passo + " ");
+        }
+        caminho = BuscaEmLargura.getInstance().buscar(g, "B", "G");
+        System.out.println();
+        System.out.print("Caminho feito por uma busca em largura: ");
+        for(String passo : caminho) {
+         System.out.print(passo + " ");
+        }
+        System.out.println("");
+
     }
 }
